5da2298eb99afc729a3cd38b9754a9c4a17b5a1c
Vvitali <vvitali26@gmail.com>
2018-03-09 14:34:53 -0500

    jquery 0.1

---
+++ js/jQuery.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function(){
+  // Some code
+});
+
+$(document).ready(function(){
+  // Some code
+});
+
+$(function(){
+   // Some code
+});
+
+jQuery(function(){
+   // Some code
+});
+
+//Each of function above - do the same thing. 
+
+
+$("*")
+$("Element")
+$("#id")
+$(".class")
+$("selector1,selector2,selectorN")
+
+
+// selectors
